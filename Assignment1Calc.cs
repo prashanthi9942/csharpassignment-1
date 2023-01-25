89bc60653d621a9ad3e68531abf80ed01e4120cd
prashanthi9942 <prashanthilakshmi302@gmail.com>
2023-01-25 12:57:07 +0530

    csharp

---
+++ Assignment1Calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignment1
+{
+    class Assignment1Calc
+    {
+        static void Main()
+        {
+            int num1, num2, result1, result2, result3, result4;
+            Console.WriteLine("Please Enter 2 int numbers");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            result1 = num1 + num2;
+            result2 = num1 - num2;
+            result3 = num1 * num2;
+            result4 = num1 / num2;
+            Console.WriteLine("The sum is " + result1);
+            Console.WriteLine("The difference is " + result2);
+            Console.WriteLine("The product is " + result3);
+            Console.WriteLine("The division is " + result4);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Assignment1Maxmarks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignment1
+{
+    class Assignment1Maxmarks
+    {
+        static void Main()
+        {
+            int[] grades = new int[5] { 21, 22, 23, 22, 20 };
+            int i, high, count;
+            count = 5;
+            high = grades[0];
+            for (i = 1; i < count; i++)
+            {
+                if (grades[i] > high)
+                {
+                    high = grades[i];
+                }
+            }
+            Console.WriteLine("max marks={0} ", +high);
+            Console.ReadLine();
+        }
+    }
+}
